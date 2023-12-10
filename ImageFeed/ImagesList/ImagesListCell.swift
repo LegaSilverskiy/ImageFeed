3b30a7cb6b3e652cb8de60f141cdb6891914434f
LegaSilverskiy <slepchenko98@yandex.ru>
2023-12-10 11:53:54 +0500

    Adding to the first commit

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -9,5 +9,8 @@ import Foundation
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
-    
+    static let reuseIdentifier = "ImagesListCell"
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
 }
