9b2c1265ebbef416f875138b2150572a8f0865c4
LegaSilverskiy <slepchenko98@yandex.ru>
2023-12-10 11:53:06 +0500

    A new project has been created. The launch screen has been created. The main screen has been laid out. Added mock data.

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Олег Серебрянский on 12/8/23.
+//
+
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+}
