cadea661389ca72f358f9d9ef43aa4de54e14166
LegaSilverskiy <slepchenko98@yandex.ru>
2024-01-20 15:21:23 +0500

    Create ConstantsForUnsplash.swift + Added constants for API

---
+++ ImageFeed/Constants/ConstantsForUnsplash.swift
@@ -0,0 +1,14 @@
+//
+//  ConstantsForUnsplash.swift
+//  ImageFeed
+//
+//  Created by Олег Серебрянский on 1/20/24.
+//
+
+import Foundation
+
+let AccessKey = "grqdakf7CfqaV-UBLxP25kauZZQiFJdR7bYqhZemT5M"
+let SecretKey = "ViR7Sh_yN489K97UNOGFswekCbNV-VBkdahc9X1aJtY"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")
